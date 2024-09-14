41092ab331ae04df0386201e5322856d357d8db1
marsp0 <martin.n.spasov@gmail.com>
2024-09-14 12:49:24 +0300

    adding frame positioning

---
+++ personal/frames.lua
@@ -0,0 +1,10 @@
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:SetScript("OnEvent", function()
+    PlayerFrame:ClearAllPoints()
+    PlayerFrame:SetPoint("CENTER", -221, -165)
+
+    TargetFrame:ClearAllPoints()
+    TargetFrame:SetPoint("CENTER", 221, -165)
+    print("Shiah Mods: movet player and target frames")
+end)
