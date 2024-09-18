81b04a72763a45fb4789527b7b4c7e11d43f3362
marsp0 <martin.n.spasov@gmail.com>
2024-09-18 19:18:01 +0300

    only adjust frames on login/reload

--- personal/frames.lua
+++ personal/frames.lua
@@ -1,10 +1,13 @@
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
-f:SetScript("OnEvent", function()
-    PlayerFrame:ClearAllPoints()
-    PlayerFrame:SetPoint("CENTER", -221, -165)
+f:SetScript("OnEvent", function(self, event, ...)
+    local isInitialLogin, isReloadingUi = ...
+    if isInitialLogin or isReloadingUi then
+        PlayerFrame:ClearAllPoints()
+        PlayerFrame:SetPoint("CENTER", -221, -165)
 
-    TargetFrame:ClearAllPoints()
-    TargetFrame:SetPoint("CENTER", 221, -165)
-    print("Shiah Mods: moved player and target frames")
+        TargetFrame:ClearAllPoints()
+        TargetFrame:SetPoint("CENTER", 221, -165)
+        print("Shiah Mods: moved player and target frames")
+    end
 end)
