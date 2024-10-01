0656617f7d423930c9a9415a44372993b81caf57
marsp0 <martin.n.spasov@gmail.com>
2024-10-01 16:30:31 +0300

    adjusting combat log code

--- raid/combatlog.lua
+++ raid/combatlog.lua
@@ -1,18 +1,15 @@
 function combat_log_event()
     local zoneName, instanceType, _, _, _, _, _, areaID = GetInstanceInfo()
 
-	if instanceType == "raid" then 
-		if raid_zones_map[areaID] and not LoggingCombat() then
-            print("Enabling combatlog")
-			LoggingCombat(true)
-			return
-		end
+	if instanceType == "raid" and raid_zones_map[areaID] then 
+		print("Enabling combatlog")
+		LoggingCombat(true)
+	else
+		print("Disabling combatlog")
+    	LoggingCombat(false)
 	end
-
-    print("Disabling combatlog")
-    LoggingCombat(false)
 end
 
-combatlog_frame = CreateFrame("Frame")
-combatlog_frame:SetScript("OnEvent", combat_log_event)
-combatlog_frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+local f = CreateFrame("Frame")
+f:SetScript("OnEvent", combat_log_event)
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
