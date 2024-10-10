e3b37f1f6250af76ec6ef455d5e47265cf1fe9da
marsp0 <martin.n.spasov@gmail.com>
2024-10-10 11:22:07 +0300

    toggle portal scan with command

--- personal/portals.lua
+++ personal/portals.lua
@@ -1,24 +1,24 @@
+local on = false
 local f = CreateFrame("Frame")
 f:SetScript("OnEvent", function(self, event, ...)
-    if event == "PLAYER_ENTERING_WORLD" then
+    local msg, author = ...
+    msg = string.lower(msg)
+    local is_wtb = string.find(msg, "wtb")
+    local is_port = string.find(msg, "port")
+
+    if is_wtb and is_port then
+        InviteUnit(author)
+    end
+end)
+
+SlashCmdList['PORTALS_SLASHCMD'] = function(msg)
+    if on then
         SM_print("Scanning for portal messages disabled")
         f:UnregisterEvent("CHAT_MSG_YELL")
     else
-        local msg, author = ...
-        msg = string.lower(msg)
-        local is_wtb = string.find(msg, "wtb")
-        local is_port = string.find(msg, "port")
-
-        if is_wtb and is_port then
-            InviteUnit(author)
-        end
+        SM_print("Scanning for portal messages enabled")
+        f:RegisterEvent("CHAT_MSG_YELL")
     end
-end)
-f:RegisterEvent("PLAYER_ENTERING_WORLD")
-
-
-SlashCmdList['PORTALS_SLASHCMD'] = function(msg)
-    SM_print("Scanning for portal messages enabled")
-    f:RegisterEvent("CHAT_MSG_YELL")
+    on = not on
 end
 SLASH_PORTALS_SLASHCMD1 = '/portals'
