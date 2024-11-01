4cee21b00e23d759a43bd0d79db1a980d26cb9e0
marsp0 <martin.n.spasov@gmail.com>
2024-11-01 18:22:24 +0200

    enable spell showing

--- personal/spells.lua
+++ personal/spells.lua
@@ -1,5 +1,5 @@
+local on = false
 local f = CreateFrame("Frame")
-f:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 f:SetScript("OnEvent", function(self, event, ...) 
     local unit_id, spell_guid, spell_id = ...
     if (unit_id ~= "player") then return end
@@ -7,3 +7,15 @@ f:SetScript("OnEvent", function(self, event, ...)
     local name = GetSpellInfo(spell_id)
     print(name)
 end)
+
+SlashCmdList['SPELLS_SLASHCMD'] = function(msg)
+    if on then
+        SM_print("Showing casted spells disabled")
+        f:UnregisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+    else
+        SM_print("Showing casted spells enabled")
+        f:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+    end
+    on = not on
+end
+SLASH_SPELLS_SLASHCMD1 = '/spells'
