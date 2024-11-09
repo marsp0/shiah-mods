e3a8f0aba178ee9a045cace9fd24745be31f6b37
marsp0 <martin.n.spasov@gmail.com>
2024-11-09 11:27:15 +0200

    adjusting target of target codei n tooltip

--- personal/tooltip.lua
+++ personal/tooltip.lua
@@ -15,15 +15,6 @@ GameTooltip:HookScript("OnTooltipSetUnit", function(self)
 
     local line1 = _G["GameTooltipTextLeft1"]
     local line2 = _G["GameTooltipTextLeft2"]
-    local line3 = _G["GameTooltipTextLeft3"]
-    local line4 = _G["GameTooltipTextLeft4"]
-    local line5 = _G["GameTooltipTextLeft5"]
-
-    line1:SetText("")
-    line2:SetText("")
-    line3:SetText("")
-    line4:SetText("")
-    line5:SetText("")
 
     if unit_is_player then
         local unit_color = RAID_CLASS_COLORS[unit_class]:GenerateHexColorMarkup()
@@ -35,25 +26,22 @@ GameTooltip:HookScript("OnTooltipSetUnit", function(self)
         local guild, guild_rank = GetGuildInfo(unit)
         if not guild then return end
         local guild_line = "|c00aaaaff" .. guild .. " - " .. guild_rank
-        if GameTooltip:NumLines() >= 3 and guild ~= GetGuildInfo("player") then
-            line3:SetText(guild_line)
-        else
-            GameTooltip:AddLine(guild_line)
-        end
+        GameTooltip:AddLine(guild_line)
+    end
 
-        -- target of target
-        local tt_unit   = "mouseovertarget"
-        local tt_name   = GetUnitName(tt_unit)
+    -- target of target
+    local tt_unit       = unit .. "target"
+    local tt_name       = GetUnitName(tt_unit)
 
-        if not tt_name then return end
+    if not tt_name then return end
 
-        local tt_class  = UnitClassBase(tt_unit)
-        local tt_color  = RAID_CLASS_COLORS[tt_class]:GenerateHexColorMarkup()
+    local tt_is_player  = UnitIsPlayer(tt_unit)
+    local tt_class      = UnitClassBase(tt_unit)
+    local tt_color      = RAID_CLASS_COLORS[tt_class]:GenerateHexColorMarkup()
+
+    if tt_is_player then
         GameTooltip:AddLine("Target: " .. tt_color .. tt_name)
     else
-        line1:SetText(unit_name)
-        line2:SetText("Level " .. unit_level)
-
-        if UnitPlayerControlled(unit) then line3:SetText("") end
+        GameTooltip:AddLine("Target: " .. tt_name)
     end
 end)
