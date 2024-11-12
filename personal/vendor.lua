a0ac5d55845a6657802f92f6717e98a81416b676
marsp0 <martin.n.spasov@gmail.com>
2024-11-12 18:26:32 +0200

    auto sell trash

--- personal/vendor.lua
+++ personal/vendor.lua
@@ -2,6 +2,21 @@
 local f = CreateFrame("Frame")
 f:RegisterEvent("MERCHANT_SHOW")
 f:SetScript("OnEvent", function(self, event, ...)
+    -- auto sell trash
+    for bag = 0, 4 do 
+        for slot = 1, C_Container.GetContainerNumSlots(bag) do 
+            local item_link = C_Container.GetContainerItemLink(bag, slot) 
+            if item_link then
+                local _, _, rarity = GetItemInfo(item_link)
+                if rarity == 0 then 
+                    DEFAULT_CHAT_FRAME:AddMessage("Selling " .. item_link) 
+                    C_Container.UseContainerItem(bag,slot) 
+                end
+            end
+        end 
+    end
+
+    -- repair
     if not CanMerchantRepair() then return end
     local cost = GetRepairAllCost()
     if GetMoney() < cost or cost == 0 then return end
