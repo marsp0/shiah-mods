dae882afafe18fd18784838c94160b67b242f530
marsp0 <martin.n.spasov@gmail.com>
2024-11-22 12:34:06 +0200

    enable note syncing in golemagg

--- epgp/epgp_updater.lua
+++ epgp/epgp_updater.lua
@@ -20,4 +20,9 @@ processing = false
 last_sync = GetTime()
 epgp_updater_frame = CreateFrame("Frame")
 epgp_updater_frame:SetScript("OnEvent", epgp_updater_sync_notes)
-epgp_updater_frame:RegisterEvent("GUILD_ROSTER_UPDATE")
+if GetRealmName() == "Golemagg" then
+    epgp_updater_frame:RegisterEvent("GUILD_ROSTER_UPDATE")
+    SM_print("[Enabled] Officer and public notes syncing")
+else
+    SM_print("[Disabled] Officer and public notes syncing")
+end
