9918cc848d3aeac85e936ec793ad9d8b0ad53b65
marsp0 <martin.n.spasov@gmail.com>
2025-03-01 15:45:23 +0200

    autoinvite: removed as not used

--- autoinvite.lua
+++
@@ -1,6 +0,0 @@
-local f = CreateFrame("Frame")
-f:RegisterEvent("CHAT_MSG_WHISPER")
-f:SetScript("OnEvent", function(self, event, ...)
-    local msg, author = ...
-    if string.lower(msg) == "inv" then InviteUnit(author) end
-end)
