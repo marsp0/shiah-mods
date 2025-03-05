c88d9b05a09da45978e6310f4b3c950e4393f177
marsp0 <martin.n.spasov@gmail.com>
2025-03-05 16:09:23 +0200

    questie: bug fix quest_log_id retrieval

--- questie.lua
+++ questie.lua
@@ -5,7 +5,7 @@ for i = 1, 6 do
     f:SetScript("OnClick", function(self, ...)
         handler_map[i](self, ...)
         if select(1, ...) ~= "RightButton" or self.isHeader then return end
-        local quest_id = select(8, GetQuestLogTitle(i))
+        local quest_id = select(8, GetQuestLogTitle(QuestLogListScrollFrame.offset + i))
         ChatEdit_ActivateChat(DEFAULT_CHAT_FRAME.editBox)
         DEFAULT_CHAT_FRAME.editBox:SetText("https://www.wowhead.com/classic/quest=" .. quest_id)
     end)
