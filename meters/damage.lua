af23cb0e2cf39102950b462ed8a0db1d053e9fb0
marsp0 <martin.n.spasov@gmail.com>
2025-04-14 14:55:22 +0300

    meters: refactoring

---
+++ meters/damage.lua
@@ -0,0 +1,169 @@
+local empty_db = { ["Overall"] = {}, ["Current"] = {}}
+SM_dmg_db = SM_dmg_db or empty_db
+local current_view = "Overall"
+local player_name = UnitName("player")
+local player_class = select(2, UnitClass("player"))
+local combat_end = 0
+local recent_cleared = false
+local last_draw = 0
+
+local dmg_events = { ["SWING_DAMAGE"] = true, ["RANGE_DAMAGE"] = true,
+                     ["SPELL_DAMAGE"] = true, ["DAMAGE_SHIELD"] = true,
+                     ["DAMAGE_SPLIT"] = true, ["SPELL_PERIODIC_DAMAGE"] = true }
+
+local function damage_tooltip_show(self)
+    GameTooltip:SetOwner(self)
+    
+    local unit      = self.unit
+    if not unit then return end
+    
+    local max_dmg   = SM_dmg_db[current_view][unit]["Total"]
+    if max_dmg == 0 then return end
+
+    GameTooltip:AddDoubleLine("Total", max_dmg)
+    GameTooltip:AddLine(" ")
+
+    local names = {}
+    for k, v in pairs(SM_dmg_db[current_view][unit]) do 
+        if k ~= "Total" then table.insert(names, k) end
+    end
+    table.sort(names, function(a, b) return SM_dmg_db[current_view][unit][a] > SM_dmg_db[current_view][unit][b] end)
+
+    for i, name in ipairs(names) do
+        local dmg = SM_dmg_db[current_view][unit][name]
+        local percent = tonumber(string.format("%.1f", (dmg / max_dmg * 100)))
+        if percent >= 0.1 then
+            GameTooltip:AddDoubleLine("|cffffffff" .. i .. ". " .. name, "|cffffffff" .. dmg .. " (" .. percent .. "%)")
+        end
+    end
+
+    GameTooltip:Show()
+end
+
+local function dps_add_data(db, name, spell_name, amount)
+    db[name] = db[name] or {}
+    db[name][spell_name] = db[name][spell_name] or 0
+    db[name][spell_name] = db[name][spell_name] + amount
+    db[name]["Total"] = db[name]["Total"] or 0
+    db[name]["Total"] = db[name]["Total"] + amount
+end
+
+local function damage_assign_unit_to_bar(bar, index, name, color, value, max_value)
+    bar.left:SetText(index .. ". " .. string.sub(name, 1, 8))
+    bar.right:SetText(value)
+    bar.unit = name
+    bar:SetValue(value / max_value * 100)
+    bar:SetStatusBarColor(color.r, color.g, color.b, 1)
+end
+
+local function damage_clear_bars(window)
+    for i, bar in pairs(window.bars) do 
+        bar.left:SetText("")
+        bar.right:SetText("")
+        bar.unit = nil
+        bar:SetValue(0)
+    end
+end
+
+local function damage_swap_view(self, new_view)
+    damage_clear_bars(self)
+    current_view = new_view
+    self.title:SetText(current_view)
+end
+
+local function damage_log_event(self)
+    local timestamp, subevent = CombatLogGetCurrentEventInfo()
+    if not dmg_events[subevent] then return end
+    
+    combat_end = GetTime()
+    recent_cleared = false
+    if current_view == "Overall" then damage_swap_view(self, "Current") end
+    
+    local _, event, hideCaster, sourceGUID, sourceName, _, _, destGUID, destName = CombatLogGetCurrentEventInfo()
+    if not (sourceName and UnitIsPlayer(sourceName)) then return end
+
+    local amount = 10000
+    local overkill = 0
+    local spell_name = "DEBUG"
+    local spell_id = 0
+    if event == "SWING_DAMAGE" then
+        spell_name  = "Attack"
+        amount      = select(12, CombatLogGetCurrentEventInfo())
+        overkill    = select(13, CombatLogGetCurrentEventInfo())
+    else
+        spell_id    = select(12, CombatLogGetCurrentEventInfo())
+        spell_name  = select(13, CombatLogGetCurrentEventInfo())
+        amount      = select(15, CombatLogGetCurrentEventInfo())
+        overkill    = select(16, CombatLogGetCurrentEventInfo())
+    end
+
+    dps_add_data(SM_dmg_db["Current"], sourceName, spell_name, amount - overkill)
+
+    if spell_id == 12654 then return end -- ignore ignite damage in overall data
+    dps_add_data(SM_dmg_db["Overall"], sourceName, spell_name, amount - overkill)
+end
+
+function damage_on_update(self)
+    local now = GetTime()
+    if now - last_draw < 0.25 then return end
+
+    if now - combat_end > 5 and not recent_cleared then
+        SM_dmg_db["Current"] = {}
+        recent_cleared = true
+        damage_swap_view(self, "Overall")
+        print("cleared current segment")
+    end
+
+    -- sort names by damage
+    local names = {}
+    for k, v in pairs(SM_dmg_db[current_view]) do table.insert(names, k) end
+    table.sort(names, function(a, b) return SM_dmg_db[current_view][a]["Total"] > SM_dmg_db[current_view][b]["Total"] end)
+
+    for i, name in ipairs(names) do
+        if i > 10 then break end
+        local bar = self.bars[i]
+        local dmg = SM_dmg_db[current_view][name]["Total"]
+        local max_dmg = SM_dmg_db[current_view][names[1]]["Total"]
+        local _, class = UnitClass(name);
+        local color = RAID_CLASS_COLORS[class] or {r = 0.5, g = 0.5, b = 0.5}
+        damage_assign_unit_to_bar(bar, i, name, color, dmg, max_dmg)
+    end
+
+    for i, name in ipairs(names) do
+        if name == player_name and i > 10 then
+            local bar = self.bars[10]
+            local dmg = SM_dmg_db[current_view][name]["Total"]
+            local max_dmg = SM_dmg_db[current_view][names[1]]["Total"]
+            local _, class = UnitClass(name);
+            damage_assign_unit_to_bar(bar, i, player_name, RAID_CLASS_COLORS[class], dmg, max_dmg)
+        end
+    end
+
+    last_draw = now
+end
+
+local function damage_enter_world(self)
+    if IsInInstance() then 
+        self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+        self:SetScript("OnUpdate", damage_on_update)
+    else 
+        self:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED") 
+        self:SetScript("OnUpdate", nil)
+    end
+end
+
+local frame = meters_create_table("DamageMeter", 162, 0, damage_tooltip_show)
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:SetScript("OnEvent", function(self, event)
+    if event == "PLAYER_ENTERING_WORLD" then damage_enter_world(self) end
+    if event == "COMBAT_LOG_EVENT_UNFILTERED" then damage_log_event(self) end
+end)
+
+SlashCmdList['CLEAR_SLASHCMD'] = function(msg)
+    SM_dmg_db = {}
+    SM_dmg_db["Overall"] = {}
+    SM_dmg_db["Current"] = {}
+    damage_clear_bars(window)
+    SM_print("DPS database cleared")
+end
+SLASH_CLEAR_SLASHCMD1 = '/clear'
---
+++ meters/window.lua
@@ -0,0 +1,86 @@
+local backdrop_window = { bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
+                          tile = true, tileSize = 16, edgeSize = 16,
+                          insets = { left = 3, right = 3, top = 3, bottom = 3 } }
+local backdrop_border = { edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+                          tile = true, tileSize = 16, edgeSize = 16,
+                          insets = { left = 3, right = 3, top = 3, bottom = 3 } }
+
+local total_width = 322
+local table_width = 160
+local bar_width = 150
+local bar_height = 16
+local title_height = 22
+local created = false
+
+local window = CreateFrame("Frame", "SMMeterWindow", UIParent, "BackdropTemplate")
+window:RegisterEvent("PLAYER_ENTERING_WORLD")
+window:EnableMouse(true)
+window:RegisterForDrag("LeftButton")
+window:SetMovable(true)
+window:SetScript("OnDragStart", function() window:StartMoving() end)
+window:SetScript("OnDragStop", function() window:StopMovingOrSizing() end)
+window:SetBackdrop(backdrop_window)
+window:SetBackdropColor(.5,.5,.5,.5)
+window:SetWidth(total_width)
+window:SetHeight(bar_height * 10 + title_height + 4)
+window:SetPoint("RIGHT", UIParent, "RIGHT", -150, -150)
+
+window.border = CreateFrame("Frame", "$parentBorder", window, "BackdropTemplate")
+window.border:ClearAllPoints()
+window.border:SetPoint("TOPLEFT", window, "TOPLEFT", -1,1)
+window.border:SetPoint("BOTTOMRIGHT", window, "BOTTOMRIGHT", 1,-1)
+window.border:SetFrameLevel(100)
+window.border:SetBackdrop(backdrop_border)
+window.border:SetBackdropBorderColor(.7,.7,.7,1)
+SM_print("DPSMeter UI created")
+window:SetScript("OnEvent", function(self, event)
+    if not IsInInstance() then window:Hide(); SM_print("DPSMeter disabled"); return end
+    window:Show()
+    SM_print("DPSMeter enabled"); 
+end)
+
+function meters_create_table(name, x_offset, y_offset, on_tooltip_show)
+    local table = CreateFrame("Frame", name, window)
+    table:EnableMouse(true)
+    table:SetWidth(table_width)
+    table:SetHeight(bar_height * 10 + title_height + 4)
+    table:SetPoint("TOPLEFT", window, x_offset, y_offset)
+
+    table.title = window:CreateFontString("$parentTitle", "OVERLAY", "GameFontWhite")
+    table.title:SetFont(STANDARD_TEXT_FONT, 11)
+    table.title:SetText(name)
+    table.title:SetPoint("TOP", table, 0, -8)
+
+    local create_text_bar = function(parent, name, justify)
+        local f = parent:CreateFontString(name, "OVERLAY", "GameFontNormal")
+        f:SetFont(STANDARD_TEXT_FONT, 10)
+        f:SetJustifyH(justify)
+        f:SetFontObject(GameFontWhite)
+        f:SetParent(parent)
+        f:ClearAllPoints()
+        f:SetPoint("TOPLEFT", parent, "TOPLEFT", 5, 1)
+        f:SetPoint("BOTTOMRIGHT", parent, "BOTTOMRIGHT", -5, 0)
+        return f
+    end
+
+    table.bars = {}
+    for i = 1, 10 do
+        local name = "DPSMeterBar" .. i
+        table.bars[i] = CreateFrame("StatusBar", "$parentBar" .. i, table)
+        table.bars[i]:SetWidth(bar_width)
+        table.bars[i]:SetHeight(bar_height)
+        table.bars[i]:SetMinMaxValues(0, 100)
+        table.bars[i]:SetPoint("TOPLEFT", table, "TOPLEFT", 5, -title_height - ((i-1) * bar_height))
+        table.bars[i]:SetStatusBarTexture("Interface/TargetingFrame/UI-StatusBar")
+        table.bars[i]:SetStatusBarColor(0, 0, 0, 0.0)
+        table.bars[i]:SetMinMaxValues(0, 100)
+        table.bars[i].left = create_text_bar(table.bars[i], name .. "TextLeft", "LEFT")
+        table.bars[i].right = create_text_bar(table.bars[i], name .. "TextRight", "RIGHT")
+        table.bars[i]:EnableMouse(true)
+        table.bars[i]:SetScript("OnEnter", on_tooltip_show)
+        table.bars[i]:SetScript("OnLeave", function() GameTooltip:Hide() end)
+    end
+
+    table:Show()
+    return table
+end
