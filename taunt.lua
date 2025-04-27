d44172882e28d49786faeeacc9a9aa2e1d2bd11d
marsp0 <martin.n.spasov@gmail.com>
2025-04-27 14:41:44 +0300

    taunt: adding aoe taunt notifier

---
+++ taunt.lua
@@ -0,0 +1,10 @@
+local prev = 0
+local window = CreateFrame("Frame", "Taunt", UIParent)
+window:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+window:SetScript("OnEvent", function(self, event, ...)
+    local now = GetTime()
+    if now - prev < 1 then return end
+    if select(3, ...) ~= 1161 then return end -- Taunt
+    miniwigs_announcer_set_text("AOE Taunt")
+    prev = now
+end)
