c42f4698af7615d465978ffc07772844e162404e
marsp0 <martin.n.spasov@gmail.com>
2026-03-21 21:18:26 +0200

    removing Show function for all hidden frames

--- bars.lua
+++ bars.lua
@@ -1,34 +1,23 @@
--- artframe children
-ActionBarDownButton:Hide()
-ActionBarUpButton:Hide()
-CharacterBag0Slot:Hide()
-CharacterBag1Slot:Hide()
-CharacterBag2Slot:Hide()
-CharacterBag3Slot:Hide()
-CharacterMicroButton:Hide()
-GuildMicroButton:Hide()
-HelpMicroButton:Hide()
-KeyRingButton:Hide()
-KeyRingButton.Show = function (...) return end
-MainMenuBarBackpackButton:Hide()
-MainMenuMicroButton:Hide()
-MultiBarBottomRight:Hide()
-QuestLogMicroButton:Hide()
-SocialsMicroButton:Hide()
-SpellbookMicroButton:Hide()
-TalentMicroButton:Hide()
-TalentMicroButton.Show = function (...) return end
-WorldMapMicroButton:Hide()
+local frames = {"ActionBarDownButton", "ActionBarUpButton", "CharacterBag0Slot",
+                "CharacterBag1Slot", "CharacterBag2Slot", "CharacterBag3Slot",
+                "CharacterMicroButton", "GuildMicroButton", "HelpMicroButton",
+                "KeyRingButton", "MainMenuBarBackpackButton", "MainMenuMicroButton",
+                "MultiBarBottomRight", "QuestLogMicroButton", "SocialsMicroButton",
+                "SpellbookMicroButton", "TalentMicroButton", "WorldMapMicroButton",
+                -- artframe regions
+                "MainMenuBarLeftEndCap", "MainMenuBarRightEndCap", "MainMenuBarPageNumber",
+                "MainMenuBarTexture0", "MainMenuBarTexture1", "MainMenuBarTexture2", "MainMenuBarTexture3", 
+                "MainMenuBarTextureExtender",
+                -- xp bars
+                "ReputationWatchBar", "MainMenuMaxLevelBar0", "MainMenuMaxLevelBar1", 
+                "MainMenuMaxLevelBar2", "MainMenuMaxLevelBar3",
+                "MainMenuBarMaxLevelBar", "MainMenuExpBar", "MainMenuBarPerformanceBarFrame"
+            }
 
--- artframe regions
-MainMenuBarLeftEndCap:Hide()
-MainMenuBarRightEndCap:Hide()
-MainMenuBarPageNumber:Hide()
-MainMenuBarTexture0:Hide()
-MainMenuBarTexture1:Hide()
-MainMenuBarTexture2:Hide()
-MainMenuBarTexture3:Hide()
-MainMenuBarTextureExtender:Hide()
+for i, frame in ipairs(frames) do
+    _G[frame]:Hide()
+    _G[frame].Show = function (...) return end
+end
 
 -- move MainMenuBar to the center
 local x_offset = MultiBarBottomLeft:GetWidth() / 2
@@ -37,17 +26,6 @@ MainMenuBar:SetPoint("CENTER", x_offset + 5, 0)
 -- y offset between bars the same as x offset between buttons
 local _, _, p, x, y = ActionButton2:GetPoint()
 MultiBarBottomLeft:SetPoint("TOPLEFT", 8, -x)
-
--- hide xp bars
-ReputationWatchBar:Hide()
-MainMenuMaxLevelBar0:Hide()
-MainMenuMaxLevelBar1:Hide()
-MainMenuMaxLevelBar2:Hide()
-MainMenuMaxLevelBar3:Hide()
-MainMenuBarMaxLevelBar:Hide()
-MainMenuExpBar:Hide()
-MainMenuExpBar.Show = function (...) return end
-MainMenuBarPerformanceBarFrame:Hide()
 
 RAID_CLASS_COLORS["SHAMAN"] = CreateColor(0.0, 0.44, 0.87);
 RAID_CLASS_COLORS["SHAMAN"].colorStr = RAID_CLASS_COLORS["SHAMAN"]:GenerateHexColor();
